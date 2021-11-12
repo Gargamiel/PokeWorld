b20c1874f8824d14724a58a761b548cbafafe0c7
Gargamiel <93517691+Gargamiel@users.noreply.github.com>
2021-11-12 23:54:09 +0100

    Pokémon cannot be resurected anymore to prevent bug from breaking game, still need to fix said bug.

---
+++ Source/PokeWorld/PokeWorld/Harmony_Patching/CompTargetEffect_Resurrect_DoEffectOn_Patch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace PokeWorld
+{
+	[HarmonyPatch(typeof(CompTargetEffect_Resurrect))]
+	[HarmonyPatch("DoEffectOn")]
+	public class CompTargetEffect_Resurrect_DoEffectOn_Patch
+	{
+		public static bool Prefix(Thing __1)
+		{
+			if (__1 != null && __1 is Pawn pawn && pawn.TryGetComp<CompPokemon>() != null)
+			{
+				Log.Warning("Tried to resurrect a Pokémon, this is currently bugged and therefore disabled: " + pawn.ToStringSafe());
+				return false;
+			}
+			return true;
+		}
+	}
+}
---
+++ Source/PokeWorld/PokeWorld/Harmony_Patching/ResurrectionUtility_Resurrect_Patch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace PokeWorld
+{
+    [HarmonyPatch(typeof(ResurrectionUtility))]
+    [HarmonyPatch("Resurrect")]
+    public class ResurrectionUtility_Resurrect_Patch
+    {
+		public static bool Prefix(Pawn __0)
+		{
+			if (__0 != null && __0.TryGetComp<CompPokemon>() != null)
+			{
+				Log.Warning("Tried to resurrect a Pokémon, this is currently bugged and therefore disabled: " + __0.ToStringSafe());
+				return false;
+			}
+			return true;
+		}
+	}
+}
